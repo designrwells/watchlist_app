b2a3cf5d892569eaf9024eb63128f5093654dc0c
Darrin Wells <designrwells@gmail.com>
2019-02-11 16:25:33 -0600

    header foundation complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from './components/Header/.header.js';
+import Header from './components/Header/';
 import './App.css';
 
 class App extends Component {
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './header.css';
+import logo from '../../img/logo.svg';
+import './index.css';
 
 const Header = () => {
   return (
     <div className="App-header">
       <div className="App-logo">
         <a className="App-header-title" href="/"><img src={logo} alt="Watchlist App"/></a>
-       
       </div>
     </div>
   );
